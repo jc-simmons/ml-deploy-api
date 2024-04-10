fcc82ad9b52df8cd5f6c0cd646a5471d3a95c8a5
Jesse Simmons <133896662+jc-simmons@users.noreply.github.com>
2024-04-10 11:52:15 -0400

    create preprocessor

---
+++ src/preprocess.py
@@ -0,0 +1,56 @@
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler, OneHotEncoder, LabelEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.base import BaseEstimator, TransformerMixin
+
+def create_preprocessor():
+
+
+    prepare_pipe = Pipeline([
+        ('dropna', Cleaner()),
+        ('selector', DropFeatureSelector('PatientID'))
+        ])
+
+    numeric_features = ['Pregnancies','PlasmaGlucose','DiastolicBloodPressure',
+    'TricepsThickness','SerumInsulin','BMI','DiabetesPedigree']
+
+    numeric_transformer = Pipeline([
+        ('minmax_scaler',MinMaxScaler())
+        ])
+
+
+    preprocessor = Pipeline([
+        ('prep', prepare_pipe),
+
+        ('column_transform', 
+        ColumnTransformer([
+            ('num', numeric_transformer, numeric_features)], remainder='passthrough'))
+        ])
+
+    return preprocessor
+
+
+
+class Cleaner(BaseEstimator, TransformerMixin):
+    
+    def __init__(self): 
+        return self
+
+    def fit(self, X, y=None):
+        return self 
+
+    def transform(self, X):
+        X = X.dropna()
+        return X
+
+
+class DropFeatureSelector(BaseEstimator, TransformerMixin):
+    def __init__(self, variables):
+        self.variables = variables
+
+    def fit(self, X, y = None):
+        return self
+
+    def transform(self, X):
+        X_dropped = X.drop(self.variables, axis = 1)
+        return X_dropped
