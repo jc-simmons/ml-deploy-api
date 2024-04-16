61ff1224e8a370c5898328049785dd71fc7a5f9c
Jesse Simmons <133896662+jc-simmons@users.noreply.github.com>
2024-04-16 18:15:29 -0400

    update config

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import pandas as pd
 import pathlib
 from sklearn.model_selection import train_test_split
 
+
 from load_data import data_loader
 from preprocess import create_preprocessor
 from models import Model
@@ -12,30 +13,32 @@ from models import Model
 
 with open('config.yaml','r') as conf:
     try:
-        cfg = yaml.safe_load(conf)
+        config = yaml.safe_load(conf)
     except yaml.YAMLError as exc:
         print(exc)
 
 
 # Data configuration
-data_dir = cfg['paths']['data']
-data_file = cfg['files']['input_file']
-data_path = pathlib.Path(f"{data_dir}/{data_file}")
+DATA_PATH = pathlib.Path(f"{config['in']}")
+OUT_PATH = pathlib.Path(f"{config['out']}")
 
-data = data_loader(data_path)
+MODEL_PARAMS = config['models']
+TARGET_VAR = config['target']
 
-y = data[['Diabetic']].values.ravel()
-X = data.drop('Diabetic', axis=1)
+data = data_loader(DATA_PATH)
 
+y = data[[TARGET_VAR]].values.ravel()
+X = data.drop(TARGET_VAR, axis=1)
 
 X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.2, random_state=42)
 
-
 preprocessor = create_preprocessor()
 
-ml = Model(preprocessor, cfg['models']).gen()
+ml = Model(preprocessor, MODEL_PARAMS).gen()
 
 ml.fit(X_train, y_train)
+
+
 
 pd.set_option('display.max_colwidth', 200)
 print(ml.chop_results_)
